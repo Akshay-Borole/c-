d48ab2bf4e704cc10e8367e2b5783b009db557a5
Akshay-Borole <88107265+Akshay-Borole@users.noreply.github.com>
2021-12-01 19:18:04 +0530

    Create InputOutput.cpp

---
+++ InputOutput.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+/* Read input from STDIN. Print output to STDOUT */
+  
+    int n,s,o,sum;
+    cin>>s>>n>>o;
+    sum=s+n+o;
+    cout<<sum<<endl;  
+    return 0;
+}
